bfdcc9b72900eafec59002bcf780bbfe791c7819
sam01ln12 <63307379+sam01ln12@users.noreply.github.com>
2020-04-16 18:22:11 +0400

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var question = prompt ("Do you want taste our steak? ", "Yes" );
+if (question == "Yes") {
+    var age = prompt("How old are You?");
+    
+    if (age<21) {
+        alert("Sorry,  but You can't get our steak");
+    }
+    else {
+        alert ("Please, this is your steak!");
+    }
+}
+
+else {
+    alert("Please, here are your fri and ketchup");
+}
+
+
